78847a68715276ec9cee8d996af915e022d34d97
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-12-23 12:41:12 +0100

    Added wilder type

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+interface Wilder {
+  name: String;
+  age: number;
+}
+
 const useState = <T>(initialValue: T): [() => T, (arg: T) => void] => {
   let state = initialValue;
   const getState = () => state;
@@ -12,3 +17,10 @@ const [count, setCount] = useState(0);
 console.log(count());
 setCount(3);
 console.log(count());
+
+const [wilders, setWilders] = useState<Wilder[]>([]);
+
+setWilders([{ age: 32, name: "Karim" }]);
+console.log(wilders());
+setWilders([...wilders(), { name: "Jessy", age: 34 }]);
+console.log(wilders());
